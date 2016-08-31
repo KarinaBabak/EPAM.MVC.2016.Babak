13e3f4a502c38df62ca567d950f5a16db867ee6e
Karina <karisha.babak@gmail.com>
2016-08-31 22:46:19 +0300

    day1 start

---
+++ Day1 - Routing/WebApplication/AdditionalLibrary/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+
+namespace AdditionalLibrary
+{
+    public class HomeController : Controller
+    {       
+        [HttpGet]
+        public ActionResult Index(string id = "Default Id")
+        {            
+            string data = "Json Result from Additional controller";
+
+            return Json(data, JsonRequestBehavior.AllowGet);     
+        }
+
+        [HttpGet]
+        public ActionResult AnotherIndex(string id = "Default Id")
+        {            
+            string data = "AnotherIndex from Additional controller: ID = " + id;
+
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ Day1 - Routing/WebApplication/WebApplication/App_Start/RouteConfig.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using WebApplication.App_Start;
+
+namespace WebApplication
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+
+            routes.MapRoute("AdditionalAnotherRoute", "Additional/{controller}/{action}/{id}",
+            new
+            {
+                controller = "Home",
+                action = "AnotherIndex",
+                id = UrlParameter.Optional
+            },
+            namespaces: new[] { "AdditionalLibrary" });
+
+
+            Route homeRoute = routes.MapRoute("MyAnotherRoute", "Another/{controller}/{action}/{id}",
+            new
+            {
+                controller = "Home",
+                action = "AnotherIndex",
+                id = UrlParameter.Optional
+            },
+            new { id = new ExpectedValuesConstraint("songs", "books", "images") },
+            namespaces: new[] { "WebApplication.Controllers" });
+            homeRoute.DataTokens["UseNamespaceFallback"] = false;
+
+
+
+            routes.MapRoute("HomeRoute", "{controller}/{action}",
+            new
+            {
+                controller = "Home",
+                action = "Index"
+            },
+            namespaces: new[] { "WebApplication.Controllers" });
+
+
+            routes.MapRoute("AdditionalRoute", "{controller}/{action}/{id}",
+            new
+            {
+                controller = "Home",
+                action = "Index",
+                id = UrlParameter.Optional
+            },
+            namespaces: new[] { "AdditionalLibrary" });
+
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}",
+                defaults: new { controller = "Home", action = "Index" },
+                namespaces: new[] { "WebApplication.Controllers" });
+        }
+    }
+}
