8270c0ea127f12fe6c3307f99b8e49e174dab8a9
Karina <karisha.babak@gmail.com>
2016-09-07 00:48:53 +0300

    try testing

--- Day1/WebApplication/WebApplication.Tests/When_Home_Index.cs
+++ Day1/WebApplication/WebApplication.Tests/When_Home_Index.cs
@@ -2,7 +2,6 @@
 using Machine.Specifications;
 using System.Web;
 using System.Web.Routing;
-using WebApplication.Controllers;
 
 namespace WebApplication.Tests
 {
@@ -10,24 +9,52 @@ namespace WebApplication.Tests
     public class When_HomeController_Index_called
     {     
         private static RouteCollection routes;
-        private static RouteData result;
-        //private static string url;
-        private static string controller = "home";
-        private static string action = "index";
+        private static RouteData result;       
+        private static string controller;
+        private static string action;
+        
 
         Establish context = () =>
         {
             routes = new RouteCollection();
             RouteConfig.RegisterRoutes(routes);
-           // url = "~/Another/Home/AnotherIndex/pictures";
+            controller = "Home";
+            action = "index";
         };
 
         Because of = () =>
         {
-            result = routes.GetRouteData(TestHelper.CreateHttpContext("~/home/index/"));
+            result = routes.GetRouteData(TestHelper.CreateHttpContext("~/Home/index/"));
         };
 
         It route_should_match = () => TestHelper.TestIncomingRouteResult(result, controller, action).ShouldBeTrue();
         
+    }
+
+    [Subject("Home_Routing")]
+    public class When_AdditionalHomeController_AnotherIndex_called
+    {
+        private static RouteCollection routes;
+        private static RouteData result;
+        private static string controller;
+        private static string action;
+        private static string url;
+        
+        Establish context = () =>
+        {
+            routes = new RouteCollection();
+            RouteConfig.RegisterRoutes(routes);
+            controller = "Home";
+            action = "AnotherIndex";
+            url = "~/Additional/Home/AnotherIndex/505";
+        };
+
+        Because of = () =>
+        {
+            result = routes.GetRouteData(TestHelper.CreateHttpContext(url));
+        };
+
+        It route_should_match = () => TestHelper.TestIncomingRouteResult(result, controller, action).ShouldBeTrue();
+
     }
 }
