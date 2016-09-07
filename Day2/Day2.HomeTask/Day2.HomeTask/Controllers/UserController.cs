eec448f62ed7d822aa48ba55d12b1344f70cf565
Karina <karisha.babak@gmail.com>
2016-09-07 20:46:49 +0300

    day3 start

---
+++ Day2/Day2.HomeTask/Day2.HomeTask/Controllers/UserController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Day2.HomeTask.Models;
+using System.Threading.Tasks;
+
+namespace Day2.HomeTask.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //[HttpPost]
+        //public async Task<UserRepository> AddUser(UserViewModel newUser)
+        //{
+        //    if (newUser == null)
+        //        throw new NullReferenceException("User is not exist");
+
+        //    UserRepository repository = new UserRepository();
+        //    repository.Add(newUser);
+            
+        //}
+    }
+}
---
+++ Day2/Day2.HomeTask/Day2.HomeTask/Models/NotFoundViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Day2.HomeTask.Models
+{
+    public class NotFoundViewModel
+    {
+        public string RequestedUrl { get; set; }
+        public string ReferrerUrl { get; set; }
+    }
+}
